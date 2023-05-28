8ad56763883d1241b9618c1a266e38b866395efe
Carlos <carlos.danielrox55@gmail.com>
2023-05-28 19:43:56 -0300

    Adição de página de produtos

--- cypress/e2e/produtos.cy.js
+++ cypress/e2e/produtos.cy.js
@@ -1,1 +1,28 @@
 /// <reference types="cypress"/>
+
+describe ('Funcionalidade página de produtos', () => {
+beforeEach(() => {
+    cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
+    cy.get(':nth-child(2) > .page-numbers').click ()
+});
+    it ('Deve selecionar um produto da lista', () => {
+        cy.get('[class="product-block grid"]')
+        //.eq(2)
+        .contains ('Atomic Endurance Running ')
+        .click()
+    });
+    
+    it ('Deve adicionar o produto ao carrinho', () => {
+        var quantidade = 7
+        
+        cy.get('[class="product-block grid"]')
+        .contains ('Atomic Endurance Running ').click()
+        cy.get('.button-variable-item-S').click()
+        cy.get('.button-variable-item-Red').click ()
+        cy.get('.input-text').clear().type(quantidade)
+        cy.get('.single_add_to_cart_button').click()
+        cy.get('.dropdown-toggle > .mini-cart-items').should ('contain', quantidade)
+        cy.get('.woocommerce-message').should ('contain', quantidade + ' × “Atomic Endurance Running Tee (Crew-Neck)” foram adicionados no seu carrinho.')
+        
+    });
+});
